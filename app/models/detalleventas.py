428f3331b92f380918e8839f2bc00ec6e8ed6c1a
Jannlyk1 <jannlykmartinez@gmail.com>
2024-08-28 08:52:00 -0500

    B

--- app/models/detalleventas.py
+++
@@ -1,9 +0,0 @@
-from app import db
-from sqlalchemy.orm import relationship
-
-class  Detalleventas (db.Model):
-    __tablename__ = 'detalle_venta'
-    id = db.Column(db.Integer, primary_key=True)
-    precio = db.Column(db.String(255), nullable=False)
-    id_ventas = db.Column(db.Integer, db.ForeignKey('venta.id'))
-    id_muebles = db.Column(db.Integer, db.ForeignKey('mueble.id'))
